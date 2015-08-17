d95aae8d6e3837135a603f2257660dceda995bd4
ONYLAB <osman@jhu.edu>
2015-08-17 15:40:48 -0400

    Update plotcirclesinsquaresdens.m

--- circlesinsquares/plotcirclesinsquaresdens.m
+++ circlesinsquares/plotcirclesinsquaresdens.m
@@ -1,13 +1,23 @@
 mat = dlmread('packing.dat');
-sig = 1;
-n = mat(:,1);
-L = 2+2./mat(:,2);
-L = L * sig;
-plot(n,L,'r','LineWidth',2);
+rad = 1; %unit circles, can assume 1nm
+sig = 2*rad;
+
+n = mat(:,1); %number of circles
+L = 2+2./mat(:,2); %min edge length for sigma=2
+L = L / sig; %min edge length for sigma=1
+
+rhomax = n./(L.^2); %circles per L^2 or nm^2 in our case
+
+rhomax5 = n./(L.^2)/25; %circles per L^2 or nm^2 in our case with the assumption that sigma = 5nm
+
+semilogy(L,rhomax,'r','LineWidth',2);
 hold on
-plot(n,L*5,'LineWidth',2)
+semilogy(L,rhomax5,'LineWidth',2)
 legend('\sigma=1nm','\sigma=5nm','Location','NorthWest');
-xlabel('Number of spherical particles','FontSize',16);
+ylabel('Maximum Surface Density [molecules/nm^2]','FontSize',16);
 set(gca,'FontSize',16);
-ylabel('Square edge length [nm]','FontSize',16);
+xlabel('Square edge length [nm]','FontSize',16);
 set(gcf,'color','w');
+
+tobefit = [L rhomax]; % doesn't really have to fit this
+% one can easily observe that rhomax =~ 1/sig^2
